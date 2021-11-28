e7750ba628610bcb6f7f3ca5a05682a739ccd55e
baka da yo <dyatlovleonid@gmail.com>
2021-11-28 15:53:51 +0300

    Added play.tsx

---
+++ pages/play.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+import styles from '../styles/Play.module.css';
+
+const Play: NextPage = () => (
+    <div className={styles.container}>
+        <h1>Play</h1>
+    </div>
+);
+
+export default Play;
