48bbd14a722399a58568b2ffc7d4177129ee1652
baka da yo <dyatlovleonid@gmail.com>
2021-12-02 17:10:27 +0300

    removed next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
-import { Provider } from 'next-auth/client';
 import Layout from '../components/layout';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Provider session={pageProps.session}>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </Provider>
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
 );
 
 export default App;
